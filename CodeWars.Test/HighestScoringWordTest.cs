69a0ac12cb3871b8eeab69e7f19d138a39bb01d8
michal-panfil <it.panfil@gmail.com>
2020-02-15 18:13:34 +0100

    HighestScoringWord

---
+++ CodeWars.Test/HighestScoringWordTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeWars.Highest_Scoring_Word; 
+    
+    
+    [TestFixture]
+  public class Sample_Tests
+  {
+    private static IEnumerable<TestCaseData> testCases
+    {
+      get
+      {
+        yield return new TestCaseData("man i need a taxi up to ubud").Returns("taxi");
+        yield return new TestCaseData("what time are we climbing up to the volcano").Returns("volcano");
+        yield return new TestCaseData("take me to semynak").Returns("semynak");
+      }
+    }
+  
+    [Test, TestCaseSource("testCases")]
+    public string Test(string s) => HighestScoringWord.High(s);
+  }
+
+namespace CodeWars.Test
+{
+    class HighestScoringWordTest
+    {
+        [TestFixture]
+        public class Sample_Tests
+        {
+            private static IEnumerable<TestCaseData> testCases
+            {
+                get
+                {
+                    yield return new TestCaseData("man i need a taxi up to ubud").Returns("taxi");
+                    yield return new TestCaseData("what time are we climbing up to the volcano").Returns("volcano");
+                    yield return new TestCaseData("take me to semynak").Returns("semynak");
+                }
+            }
+
+            [Test, TestCaseSource("testCases")]
+            public string Test(string s) => HighestScoringWord.High(s);
+        }
+    }
+}
---
+++ CodeWars/Highest_Scoring_Word/HighestScoringWord.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Highest_Scoring_Word
+{
+    public static class HighestScoringWord
+    {
+        public static string High(string s)
+        {
+            var words = s.Split(' ');
+            var highestword = "";
+            var highestscore = 0;
+
+            foreach(var word in words)
+            {
+                var score = 0;
+                foreach(var c in word)
+                {
+                    score += (Convert.ToSByte(c) - 96);
+                }
+                Console.WriteLine($"{word} : {score}");
+                if(highestscore < score)
+                {
+                    highestscore = score;
+                    highestword = word;
+                }
+            }
+
+            return highestword;
+        }
+    }
+}
